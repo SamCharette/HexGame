107c71b5b882938ae8b5c95b55f8fbb94e00ca65
Sam Charette <sam.charette@greatersudbury.ca>
2021-01-20 07:59:44 -0500

    WIP

--- src/Engine/Players/PlayerFactory.cs
+++ src/Engine/Players/PlayerFactory.cs
@@ -1,19 +1,49 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace Engine.Players
 {
-    public static class PlayerFactory
+    public class PlayerFactory
     {
-        public static Dictionary<string, Type> GetPlayerDictionary()
+        public List<PlayerType> PlayerTypes { get; private set; }
+        public Assembly Assembly { get; private set; }
+        public PlayerFactory(Assembly assembly = null)
         {
-            throw new NotImplementedException();
+            Assembly = assembly ?? Assembly.GetExecutingAssembly();
+            PlayerTypes = GetPlayerTypes();
+        }
+        private List<PlayerType> GetPlayerTypes()
+        {
+            var iterations = 5;
+            var pType = typeof(Player);
+            return Enumerable.Range(1, iterations)
+                .SelectMany(i => Assembly.GetExecutingAssembly().GetTypes()
+                    .Where(t => t.IsClass && t != pType
+                                          && pType.IsAssignableFrom(t))
+                    .Select(t => new PlayerType( t.Name, t.ReflectedType))).ToList();
         }
 
         public static Player CreatePlayerOfType(string playerType)
         {
             throw new NotImplementedException();
         }
+
+        public class PlayerType
+        {
+            public string Name { get; private set; }
+            public Type Type { get; private set; }
+            public PlayerType(string name, Type type)
+            {
+                Type = type;
+                Name = name;
+            }
+        }
+
     }
+
+
 }
---
+++ test/Engine.Tests/PlayerTypesTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Reflection;
+using Engine.Players;
+using NUnit.Framework;
+
+namespace Engine.Tests
+{
+    
+    public class PlayerTypesTests
+    {
+        [Test]
+        public void GetPlayerTypes_ShouldGetOneType_BecauseThereIsOnlyOneType()
+        {
+            // Assemble
+            var assemblies = Assembly
+                .GetExecutingAssembly()
+                .GetReferencedAssemblies()
+                .Select(Assembly.Load)
+                .Where(x => x.GetName() == "Engine")
+                .ToList();
+
+
+            // Act
+            var factory = new PlayerFactory();
+
+            // Assert
+            Assert.AreEqual(1, assemblies.Count());
+            Assert.AreEqual(1, factory.PlayerTypes.Count());
+        }
+    }
+}
