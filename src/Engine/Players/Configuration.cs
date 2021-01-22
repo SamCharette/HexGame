0bd54e4352537287d5ad4d3d23ee46260f96b359
Sam Charette <sam.charette@greatersudbury.ca>
2021-01-22 08:00:52 -0500

    Added configuration class that is instantiated via json, and tests

---
+++ src/Engine/Players/Configuration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Engine.Players
+{
+    public class Configuration
+    {
+        public string TypeName { get; set; }
+        public string PlayerName { get; set; }
+        public Dictionary<string, string> Options { get; set; } 
+            = new Dictionary<string, string>();
+        
+        public Configuration(string typeName, string playerName)
+        {
+            TypeName = typeName;
+            PlayerName = playerName;
+        }
+
+        public static Configuration GetConfiguration(string jsonData)
+        {
+            var data = JsonConvert.DeserializeObject<Configuration>(jsonData);
+            return data;
+        }
+    }
+}
---
+++ test/Engine.Tests/PlayerConfigurationTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Engine.Players;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Engine.Tests
+{
+    
+    public class PlayerConfigurationTests
+    {
+        [Test]
+        public void CreateFromJson_ShouldFail_IfJsonNotFormattedRight()
+        {
+            // assemble
+            var json = "no json here";
+
+            // act & assert
+            Assert.Throws<JsonReaderException>(() => Configuration.GetConfiguration(json));
+        }
+
+        [Test]
+        public void CreateFromJson_ShouldCreateObject_IfJsonIsFormattedRight()
+        {
+            // assemble
+            var json = "{\"TypeName\" : \"Player\",\"PlayerName\" : \"Test Player\"}";
+
+            // act
+            var config = Configuration.GetConfiguration(json);
+
+            // assert
+            Assert.AreEqual(typeof(Configuration), config.GetType());
+            Assert.AreEqual("Player", config.TypeName);
+            Assert.AreEqual("Test Player", config.PlayerName);
+            Assert.AreEqual(0, config.Options.Count);
+        }
+
+        [Test]
+        public void CreateFromJson_ShouldCreateObjectWithOptions_IfJsonIsFormattedRight()
+        {
+            // assemble
+            var json = "{\"TypeName\" : \"Player\",\"PlayerName\" : \"Test Player\",\"options\" : {\"First option\" : \"is an option\",\"Second option\" : \"is also an option\"}}";
+
+            // act
+            var config = Configuration.GetConfiguration(json);
+
+            // assert
+            Assert.AreEqual(typeof(Configuration), config.GetType());
+            Assert.AreEqual("Player", config.TypeName);
+            Assert.AreEqual("Test Player", config.PlayerName);
+            Assert.AreEqual(2, config.Options.Count);
+            Assert.AreEqual("is an option", config.Options.FirstOrDefault(x => x.Key == "First option").Value);
+        }
+    }
+}
