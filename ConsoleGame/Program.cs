679643896a35e9faa40709c5124bd9b256c261ea
Sam Charette <sam.charette@greatersudbury.ca>
2021-01-29 12:53:14 -0500

    Added moves to test player in console game

--- ConsoleGame/Program.cs
+++ ConsoleGame/Program.cs
@@ -9,12 +9,24 @@ namespace ConsoleGame
     {
         static void Main(string[] args)
         {
-            var size = 11;
+            var size = 5;
             var player1Args = new PlayerConstructorArguments(size, PlayerNumber.FirstPlayer);
-            var player1 = new PlayerFactory().CreatePlayerOfType("TestPlayer", player1Args);
+            TestPlayer player1 = (TestPlayer)new PlayerFactory().CreatePlayerOfType("TestPlayer", player1Args);
+            player1
+                .AddMove(0, 0)
+                .AddMove(0, 1)
+                .AddMove(0, 2)
+                .AddMove(0, 3)
+                .AddMove(0, 4);
 
             var player2Args = new PlayerConstructorArguments(size, PlayerNumber.SecondPlayer);
-            var player2 = new PlayerFactory().CreatePlayerOfType("TestPlayer", player2Args);
+            TestPlayer player2 = (TestPlayer)new PlayerFactory().CreatePlayerOfType("TestPlayer", player2Args);
+            player2
+                .AddMove(2, 0)
+                .AddMove(3, 0)
+                .AddMove(4, 0)
+                .AddMove(2, 3)
+                .AddMove(2, 2);
 
             var game = new Game(size, player1, player2);
             Console.WriteLine("Game is starting now.");
