fa4d8f6036e8d572a80633ed9c297ad591af378b
Sam Charette <sam.charette@greatersudbury.ca>
2021-01-29 12:49:02 -0500

    Added a console game interface to begin with

---
+++ ConsoleGame/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Engine;
+using Engine.Players;
+using Engine.ValueTypes;
+
+namespace ConsoleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = 11;
+            var player1Args = new PlayerConstructorArguments(size, PlayerNumber.FirstPlayer);
+            var player1 = new PlayerFactory().CreatePlayerOfType("TestPlayer", player1Args);
+
+            var player2Args = new PlayerConstructorArguments(size, PlayerNumber.SecondPlayer);
+            var player2 = new PlayerFactory().CreatePlayerOfType("TestPlayer", player2Args);
+
+            var game = new Game(size, player1, player2);
+            Console.WriteLine("Game is starting now.");
+
+            game.StartGame();
+
+            Console.WriteLine("Game over.  Player "  + game.Winner + " won.");
+        }
+    }
+}
